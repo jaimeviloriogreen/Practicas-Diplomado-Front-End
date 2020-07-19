919f9940d06effd1be2bc57992c3e2b1c6695c75
jaimeviloriogreen <jaimeviloriogreen@gmail.com>
2020-07-19 03:39:45 -0400

    Formulario con JavaScript

---
+++ Formulario-JavaScript/assets/js/codigo.js
@@ -0,0 +1,21 @@
+'use strict'
+
+let formulario = document.querySelector('.formulario');
+    
+formulario.addEventListener('submit', function(e){
+     e.preventDefault();
+     let nombre = document.querySelector('.nombre').value;
+     let correo = document.querySelector('.correo').value;
+     let asunto = document.querySelector('.asunto').value;
+     let mensaje = document.querySelector('.mensaje').value;
+
+     if(nombre == "" || correo == "" || asunto == "" || mensaje == ""){
+      alert('¡No se aceptan campos vacíos!')
+          
+     }else{
+        console.log(`El Sr. ${nombre} ha enviado un mensaje. El asunto es: ${asunto}. Su correo electrónico es: ${correo} y le ha enviado el siguiente mensaje: ${mensaje}`);
+         formulario.reset();
+         alert('¡Mensaje enviado exitosamente!');
+     }
+
+});
